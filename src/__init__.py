432598ad4b7fecd2e7d51589161230600cab81b2
Erin Anthony <erinmadisonanthony@gmail.com>
2022-04-28 12:45:32 -0400

    setup user registration

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import os
+
+import sqlalchemy
 from src.auth import auth
 from src.bookmarks import bookmarks
 from src.database import db
@@ -10,7 +12,8 @@ def create_app(test_config=None):
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI")
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False
         )
     else:
         app.config.from_mapping(test_config)    
