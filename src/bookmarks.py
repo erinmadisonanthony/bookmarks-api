19a6262c2d9efc2d320b0116c6b7f6408692b7b7
Erin Anthony <erinmadisonanthony@gmail.com>
2022-05-02 16:23:30 -0400

    setup edits and deletes

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 import validators
-from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
 from flask.json import jsonify
 from src.database import Bookmark, db
 from flask_jwt_extended import get_jwt_identity
@@ -93,3 +93,52 @@ def get_bookmark(id):
         "created_at": bookmark.created_at,
         "updated_at": bookmark.updated_at
     }), HTTP_200_OK    
+
+@bookmarks.put("/<int:id>")   
+@bookmarks.patch("/<int:id>") 
+@jwt_required()
+def editbookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({"message": "Item not found"}), HTTP_404_NOT_FOUND
+
+    body = request.get_json().get("body", "")
+    url = request.get_json().get("url", "")
+
+    if not validators.url(url):
+        return jsonify({
+            "error": "Enter a valid url"
+        }), HTTP_400_BAD_REQUEST
+
+    bookmark.url = url
+    bookmark.body = body
+
+    db.session.commit() 
+
+    return jsonify({
+        "id": bookmark.id,
+        "url": bookmark.url,
+        "short_url": bookmark.short_url,
+        "visit": bookmark.visits,
+        "body": bookmark.body,
+        "created_at": bookmark.created_at,
+        "updated_at": bookmark.updated_at
+    }), HTTP_200_OK     
+
+@bookmarks.delete("/<int:id>")
+@jwt_required()
+def delete_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({"message": "Item not found"}), HTTP_404_NOT_FOUND
+
+    db.session.delete(bookmark)
+    db.session.commit()
+    
+    return jsonify({}), HTTP_204_NO_CONTENT
