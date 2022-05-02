ad209ff83ee3374e466bc6e65b01ba34d2540b64
Erin Anthony <erinmadisonanthony@gmail.com>
2022-05-02 17:29:16 -0400

    setup link tracking and error handling

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, jsonify
 import os
 import sqlalchemy
 from src.auth import auth
 from src.bookmarks import bookmarks
-from src.database import db
+from src.constants.http_status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
+from src.database import db, Bookmark
 from flask_jwt_extended import JWTManager
 
 def create_app(test_config=None):
@@ -26,5 +27,23 @@ def create_app(test_config=None):
 
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
+
+    @app.get("/<short_url>")
+    def redirect_to_url(short_url):
+        bookmark = Bookmark.query.filter_by(short_url=short_url).first_or_404()
+
+        if bookmark:
+            bookmark.visits = bookmark.visits + 1
+            db.session.commit()
+
+            return redirect(bookmark.url)
+
+    @app.errorhandler(HTTP_404_NOT_FOUND)
+    def handle_404(e):
+        return jsonify({"error": "Not found"}), HTTP_404_NOT_FOUND
+
+    @app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
+    def handle_500(e):
+        return jsonify({"error": "Something went wrong"}), HTTP_500_INTERNAL_SERVER_ERROR           
 
     return app    
